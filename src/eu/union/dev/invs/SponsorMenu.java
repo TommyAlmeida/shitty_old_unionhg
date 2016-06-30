928469da0bca857526d02fb55e086cee52d657bd
FentisDev <fentisdev@hotmail.com>
2016-06-30 14:54:15 -0300

    Sponsor mode terminado

---
+++ src/eu/union/dev/invs/SponsorMenu.java
@@ -0,0 +1,114 @@
+package eu.union.dev.invs;
+
+import eu.union.dev.HGManager;
+import eu.union.dev.KitManager;
+import eu.union.dev.api.Icon;
+import eu.union.dev.listeners.Icons.SponsorMode;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+/**
+ * Created by Fentis on 30/06/2016.
+ */
+public class SponsorMenu implements Listener{
+
+    public void setItems(Player p, Inventory inv, int page) {
+        KitManager km = KitManager.getManager();
+
+        inv.clear();
+        int slot = 0;
+        int nextpage = page + 1;
+        int backpage = page - 1;
+        {
+            inv.setItem(slot++, new Icon(Material.CARPET, "§7Back Page").build());
+            inv.setItem(slot++, createItem(Material.STAINED_GLASS_PANE, 1, (byte) 7, "§7"));
+            inv.setItem(slot++, createItem(Material.STAINED_GLASS_PANE, 1, (byte) 7, "§7"));
+            inv.setItem(slot++, createItem(Material.STAINED_GLASS_PANE, 1, (byte) 7, "§7"));
+            inv.setItem(slot++, createItem(Material.CAKE,1,(byte)0,"§bSponsor"));
+            inv.setItem(slot++, createItem(Material.STAINED_GLASS_PANE, 1, (byte) 7, "§7"));
+            inv.setItem(slot++, createItem(Material.STAINED_GLASS_PANE, 1, (byte) 7, "§7"));
+            inv.setItem(slot++, createItem(Material.STAINED_GLASS_PANE, 1, (byte) 7, "§7"));
+        }
+        inv.setItem(slot++, new Icon(Material.CARPET, "§aNext Page", "§5Page §6" + nextpage).build());
+        inv.setItem(0, new Icon(Material.CARPET, "§7Back Page", "§5Page §6" + backpage).build());
+        int players = 0;
+        for (int i = 0; i < HGManager.getInstance().getPlayersVivos().size(); i++) {
+            if (players <=44 && page ==1){
+                Player pa = HGManager.getInstance().getPlayersVivos().get(i);
+                inv.setItem(slot++, createSkull(pa,"§a"+pa.getDisplayName()));
+            }
+            if (players >=45 && page ==2){
+                Player pa = HGManager.getInstance().getPlayersVivos().get(i);
+                inv.setItem(slot++, createSkull(pa,"§a"+pa.getDisplayName()));
+            }
+            players++;
+        }
+    }
+
+    @EventHandler
+    public void onInvClick(InventoryClickEvent e) {
+        Player p = (Player) e.getWhoClicked();
+        ItemStack item = e.getCurrentItem();
+        if (e.getInventory().getName().equalsIgnoreCase("Sponsor Player")) {
+            if (e.getSlot() < 0) {
+                return;
+            }
+            if (e.getSlot() >= 9){
+                String ps = item.getItemMeta().getDisplayName().replace("§a", "");
+                Player pl = Bukkit.getPlayer(ps);
+                if (pl != null) {
+                    Inventory inv = Bukkit.createInventory(null,9*6,"Sponsor");
+                    new SponsorMode().setItems(p,pl,inv);
+                    p.openInventory(inv);
+                    e.getView().close();
+                    e.setCancelled(true);
+                }
+            }
+            if (e.getSlot() == 0){
+                String nome = item.getItemMeta().getLore().get(0);
+                if (nome.contains("§5Page §6")) {
+                    int page = Integer.parseInt(nome.replace("§5Page §6", ""));
+                    if (page >= 1) {
+                        setItems(p, e.getClickedInventory(), page);
+                    }
+                }
+            }
+            if (e.getSlot() == 8){
+                String name = item.getItemMeta().getLore().get(0);
+                if (name.contains("§5Page §6")) {
+                    int page = Integer.parseInt(name.replace("§5Page §6", ""));
+                    if (page <= 2) {
+                        setItems(p, e.getClickedInventory(), page);
+                    }
+                }
+            }
+            if (e.getSlot() <= 8){
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    public ItemStack createItem(Material m, int q, byte d, String name) {
+        ItemStack i = new ItemStack(m, q, d);
+        ItemMeta im = i.getItemMeta();
+        im.setDisplayName(name);
+        i.setItemMeta(im);
+        return i;
+    }
+    public ItemStack createSkull(Player p, String nameitem){
+        ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+        SkullMeta sm = (SkullMeta) item.getItemMeta();
+        sm.setOwner(p.getName());
+        sm.setDisplayName(nameitem);
+        item.setItemMeta(sm);
+        return item;
+    }
+}
--- src/eu/union/dev/listeners/Icons/SponsorMode.java
+++ src/eu/union/dev/listeners/Icons/SponsorMode.java
@@ -1,6 +1,10 @@
 package eu.union.dev.listeners.Icons;
 
+import eu.union.dev.HGManager;
+import eu.union.dev.api.Ability;
 import eu.union.dev.api.Icon;
+import eu.union.dev.invs.SponsorMenu;
+import eu.union.dev.utils.Util;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -12,19 +16,26 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Tomas on 29-06-2016.
  */
 public class SponsorMode implements Listener {
 
+    private static SponsorMode instance = new SponsorMode();
+
+    public static SponsorMode getInstance() {
+        return instance;
+    }
+
     private Random rand = new Random();
     private int stack = rand.nextInt(16);
 
-    private Inventory inv = Bukkit.createInventory(null, 9*4, "Sponsor");
     private ArrayList<Player> alreadyGive = new ArrayList<>();
 
     Icon portableBench = new Icon(Material.WORKBENCH, "§6Portable Workbench");
@@ -47,6 +58,9 @@ public class SponsorMode implements Listener {
         Player p = e.getPlayer();
         ItemStack item = p.getItemInHand();
 
+        if (!HGManager.getInstance().isSpec(p)){
+            return;
+        }
         if(item == null || item.getType() == Material.AIR){
             return;
         }
@@ -66,17 +80,27 @@ public class SponsorMode implements Listener {
         if(!(item.getItemMeta().getDisplayName() == "§eSponsor")){
             return;
         }
-
-        setItems(p);
+        Inventory inv = Bukkit.createInventory(null, 6*9, "Sponsor Player");
+        new SponsorMenu().setItems(p,inv,1);
         p.openInventory(inv);
     }
 
-    private void setItems(Player p){
+    public void setItems(Player p, Player p2,Inventory inv){
         for(int i = 0; i < inv.getSize(); i++){
-            inv.setItem(i, items[i]);
+            if (i<=8){
+                inv.setItem(i,new ItemStack(Material.STAINED_GLASS_PANE));
+            }else{
+                inv.addItem(items[i-8]);
+            }
         }
+        ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+        SkullMeta sm = (SkullMeta) item.getItemMeta();
+        sm.setOwner(p2.getName());
+        sm.setDisplayName("§6"+p2.getName());
+        item.setItemMeta(sm);
+        inv.setItem(4,item);
     }
-
+    Ability cooldown = new Ability(1,5, TimeUnit.MINUTES);
     @EventHandler
     public void onInvClick(InventoryClickEvent e){
         Player p = (Player) e.getWhoClicked();
@@ -86,11 +110,64 @@ public class SponsorMode implements Listener {
             if(e.getSlot() < 0){
                 return;
             }
-
+            e.setCancelled(true);
+            if (e.getSlot() >=9){
+                if (cooldown.tryUse(p)){
+                    Player p2 = null;
+                    for (Player ps : Bukkit.getOnlinePlayers()){
+                        if (ps.getName().equalsIgnoreCase(e.getInventory().getItem(4).getItemMeta().getDisplayName().replace("§6",""))){
+                            p2 = ps;
+                        }
+                    }
+                    switch (e.getSlot()){
+                        case 9:
+                            addItem(p2,items[0]);
+                            break;
+                        case 10:
+                            addItem(p2,items[1]);
+                            break;
+                        case 11:
+                            addItem(p2,items[2]);
+                            break;
+                        case 12:
+                            addItem(p2,items[3]);
+                            break;
+                        case 13:
+                            addItem(p2,items[4]);
+                            break;
+                        case 14:
+                            addItem(p2,items[5]);
+                            break;
+                        case 15:
+                            addItem(p2,items[6]);
+                            break;
+                        case 16:
+                            addItem(p2,items[7]);
+                            break;
+                        case 17:
+                            addItem(p2,items[8]);
+                            break;
+                        case 18:
+                            addItem(p2,items[9]);
+                            break;
+                        case 19:
+                            addItem(p2,items[10]);
+                            break;
+                    }
+                }else{
+                    Util.getInstance().sendCooldownMessage(p, cooldown, TimeUnit.SECONDS, true);
+                }
+            }
             //TODO: adicionar os clicks para dar os items
         }
     }
-
+    public void addItem(Player p, ItemStack item){
+        if (p.getInventory().firstEmpty() == -1){
+            p.getWorld().dropItemNaturally(p.getLocation(),item);
+        }else{
+            p.getInventory().addItem(item);
+        }
+    }
     @EventHandler
     public void onGadget(PlayerInteractEvent e){
         Player p = e.getPlayer();
