96c313c59eb66974282e29519439543544a96d1a
FentisDev <fentisdev@hotmail.com>
2016-07-13 13:48:35 -0300

    NoKit foi adicionado no menu de kits

--- src/eu/union/dev/invs/KitMenu.java
+++ src/eu/union/dev/invs/KitMenu.java
@@ -50,15 +50,17 @@ public class KitMenu implements Listener{
                 if (p.hasPermission(km.getKits().get(i).getPermission()) ||
                         km.getKitDaPartidaPlayer(p).equals(km.getKits().get(i)) ||
                         km.getKits().get(i).getPermission().equalsIgnoreCase(Perms.KIT_FREE.toString())) {
-                    if (kits <= 44 && page == 1) {
-                        Icon icon = km.getKits().get(i).getIcon();
-                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    if (!km.isKitDisable(km.getKits().get(i))){
+                        if (kits <= 44 && page == 1) {
+                            Icon icon = km.getKits().get(i).getIcon();
+                            inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                        }
+                        if (kits >= 45 && page == 2) {
+                            Icon icon = km.getKits().get(i).getIcon();
+                            inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                        }
+                        kits++;
                     }
-                    if (kits >= 45 && page == 2) {
-                        Icon icon = km.getKits().get(i).getIcon();
-                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
-                    }
-                    kits++;
                 }
             }
         }
